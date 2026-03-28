bf2a2915745ed8148b068d5836bba79b3b27262f
Maykr1 <eclark72713@gmail.com>
2026-03-28 12:02:44 -0400

    Initial Spring Boot Project Files

---
+++ homeless-shelter-availability-api/src/test/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.homeless_shelter_availability_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessShelterAvailabilityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
