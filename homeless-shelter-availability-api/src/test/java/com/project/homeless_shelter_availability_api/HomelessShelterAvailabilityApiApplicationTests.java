b33ee8f9cdc6f6805157b494bc3e0c700d5b71c6
Rian Lewis <116776256+rianxlewis@users.noreply.github.com>
2026-03-28 16:03:12 -0400

    add testcontainer dependencies to pom.xml and imports for tests (#23)
    * add testcontainer dependencies to pom.xml and imports for tests
    
    Signed-off-by: Rian Lewis <rianxlewis@gmail.com>
    
    * fix dependency version mismatch in pom.xml + missing @Testcontainers declaration.
    
    Signed-off-by: Rian Lewis <rianxlewis@gmail.com>
    
    * make api test dependent on dockerfile
    
    Signed-off-by: Rian Lewis <rianxlewis@gmail.com>
    
    * test works. approved
    
    ---------
    
    Signed-off-by: Rian Lewis <rianxlewis@gmail.com>
    Co-authored-by: Rian Lewis <rianxlewis@gmail.com>
    Co-authored-by: Robert Lovett <rjlove02@louisville.edu>

--- homeless-shelter-availability-api/src/test/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplicationTests.java
+++ homeless-shelter-availability-api/src/test/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplicationTests.java
@@ -1,10 +1,36 @@
 package com.project.homeless_shelter_availability_api;
 
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.containers.wait.strategy.Wait;
 
+@Testcontainers
 @SpringBootTest
 class HomelessShelterAvailabilityApiApplicationTests {
+	static final ImageFromDockerfile postgresImage = new ImageFromDockerfile("hsa-postgres-test:latest", false)
+		.withDockerfile(Path.of("..", "homeless-shelter-availability-data", "Dockerfile"));
+
+	@Container
+	static GenericContainer<?> postgres_container = new GenericContainer<>(postgresImage)
+		.withExposedPorts(5432)
+		.waitingFor(Wait.forListeningPort());
+
+	@DynamicPropertySource
+	static void registerProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", () ->
+			"jdbc:postgresql://" + postgres_container.getHost() + ":" + postgres_container.getMappedPort(5432) + "/HSADB");
+		registry.add("spring.datasource.username", () -> "postgres");
+		registry.add("spring.datasource.password", () -> "ZkY6tG5lp!");
+		registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+	}
 
 	@Test
 	void contextLoads() {
