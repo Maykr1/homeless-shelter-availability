4c0b0d7a25e4b5d1b14835d0280fc7a520f0dde2
Rian Lewis <rianxlewis@gmail.com>
2026-03-28 14:09:46 -0400

    add testcontainer dependencies to pom.xml and imports for tests
    Signed-off-by: Rian Lewis <rianxlewis@gmail.com>

--- homeless-shelter-availability-api/src/test/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplicationTests.java
+++ homeless-shelter-availability-api/src/test/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplicationTests.java
@@ -2,9 +2,16 @@ package com.project.homeless_shelter_availability_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 class HomelessShelterAvailabilityApiApplicationTests {
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres_container = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
