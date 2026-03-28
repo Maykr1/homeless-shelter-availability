c545cd9421aed299e27a0585b4e53d0211a5f470
Maykr1 <eclark72713@gmail.com>
2026-03-28 12:02:44 -0400

    Initial Spring Boot Project Files

---
+++ homeless-shelter-availability-api/src/main/java/com/project/homeless_shelter_availability_api/HomelessShelterAvailabilityApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.homeless_shelter_availability_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelessShelterAvailabilityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelessShelterAvailabilityApiApplication.class, args);
+	}
+
+}
