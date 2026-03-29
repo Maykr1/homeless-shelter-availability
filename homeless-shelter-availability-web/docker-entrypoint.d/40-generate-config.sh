ee5a4083d7e56270f98aa633e8da5d56e0f0a1f3
Mitchell Koski <103057623+Electrovian@users.noreply.github.com>
2026-03-29 11:20:05 -0400

    update

--- homeless-shelter-availability-web/docker-entrypoint.d/40-generate-config.sh
+++ homeless-shelter-availability-web/docker-entrypoint.d/40-generate-config.sh
@@ -19,7 +19,7 @@ existing_google_maps_api_key="$(read_existing_value GOOGLE_MAPS_API_KEY "$OUTPUT
 existing_api_base_url="$(read_existing_value API_BASE_URL "$OUTPUT")"
 
 : "${GOOGLE_MAPS_API_KEY:=${existing_google_maps_api_key:-}}"
-: "${API_BASE_URL:=${existing_api_base_url:-http://localhost:8081}}"
+: "${API_BASE_URL:=${existing_api_base_url:-/api}}"
 
 if [ -f "$TEMPLATE" ]; then
   envsubst '${GOOGLE_MAPS_API_KEY} ${API_BASE_URL}' < "$TEMPLATE" > "$OUTPUT"
